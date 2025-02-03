9908f8ff19a2bd410b05d69a361405beb78eaa17
Abdallah <abdallahahmed6520@gmail.com>
2025-02-03 14:40:30 +0200

    Enhance checkout form: update expiration year selection to dynamically load months based on selected year; adjust pagination size in product list

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -11,19 +11,19 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!: FormGroup;  // Using definite assignment assertion
 
-  totalPrice: number = 0; 
+  totalPrice: number = 0;
   totalQuantity: number = 0;
 
   creditCardMonths: number[] = [];
   creditCardYears: number[] = [];
 
-  constructor(private formBuilder: FormBuilder,private luv2ShopFormService: Luv2ShopFormService) { }
+  constructor(private formBuilder: FormBuilder, private luv2ShopFormService: Luv2ShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
-        lastName: [''], 
+        lastName: [''],
         email: ['']
       }),
       shippingAddress: this.formBuilder.group({
@@ -59,7 +59,7 @@ export class CheckoutComponent implements OnInit {
         console.log("Retrieved credit card months: " + JSON.stringify(data));
         this.creditCardMonths = data;
       });
-    
+
     // populate credit card years
     this.luv2ShopFormService.getCreditCardYears().subscribe(
       data => {
@@ -87,5 +87,28 @@ export class CheckoutComponent implements OnInit {
       console.log(customerForm.value);
       console.log("The email address is " + customerForm.value.email);
     }
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+    
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
   }
 }
